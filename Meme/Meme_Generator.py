6969d8133917b05f706938a13f38a3818ab44b13
Tautau05 <45101516+Tautau05@users.noreply.github.com>
2020-02-03 22:22:14 +0100

    Update Meme_Generator.py

--- Meme/Meme_Generator.py
+++ Meme/Meme_Generator.py
@@ -24,7 +24,6 @@ memes = requests.get('https://api.imgflip.com/get_memes').json()['data']['memes'
 async def meme(ctx, meme_id: str, *args):
   url = 'https://api.imgflip.com/caption_image'
   template_id = 0
-  template_found = False
   try:
       # Check if ID supplied
       template_id = int(meme_id)
@@ -33,18 +32,7 @@ async def meme(ctx, meme_id: str, *args):
       for meme in memes:
           if meme_id.casefold() in meme['name'].casefold():
               template_id = meme['id']
-              template_found = True
               break
-
-  # Check for valid template_id
-  if template_found == False:
-    for i in memes:
-      if i['id'] == meme_id:
-        template_found = True
-        break  
-    if template_found == False:
-      await ctx.send('Meme not found, check the ID with !meme_templates')
-      return
 
   params = dict(template_id=template_id, username=acc_username, password=acc_password)
   for i, text in enumerate(args):
@@ -53,6 +41,11 @@ async def meme(ctx, meme_id: str, *args):
 
   data = res.json()
 
+    # Check for valid template_id
+  if data['success'] == False:
+    await ctx.send('Meme not found, check the ID with !meme_templates')
+    return
+  
   print("\n\nTimestamp: " + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) +
         "\nMeme: " + str(data['data']['url']))
     
